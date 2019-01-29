6c65640fa2733bdb7cc4056544292e9f2443319b
Oluwatimi <timi@objex.ca>
2019-01-28 20:17:23 -0500

    started working on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,29 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    background: 'rebeccapurple'
+  },
+};
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
+  <div>
+    <AppBar position={'static'} style={{backgroundColor: 'rebeccapurple'}}>
+      <Toolbar>
+        <Typography variant={'h6'} color={'inherit'}>
           {siteTitle}
-        </Link>
-      </h1>
-    </div>
+        </Typography>
+        <Button style={{position: 'absolute', right: 5}} color={'inherit'}>Sample Button</Button>
+      </Toolbar>
+    </AppBar>
   </div>
 )
-
 export default Header
