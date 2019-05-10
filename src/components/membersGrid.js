3afe750473effd14abea678986c11626df25bae9
ianDesson <ian.desson@outlook.com>
2019-05-10 01:39:21 -0400

    Refactors membersGrid to a PureComponent

--- src/components/membersGrid.js
+++ src/components/membersGrid.js
@@ -7,214 +7,221 @@ import createPalette from '@material-ui/core/styles/createPalette';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 import Helmet from 'react-helmet'
 
-const currentMembers = [
-  {
-    name: 'Anushka Paliwal',
-    title: 'President',
-    tenure: '2015-Present',
-    photoFile: 'default-female-photo.jpg',
-  },
-  {
-    name: 'Tolu Olubode',
-    title: 'Vice President',
-    tenure: '2017-Present',
-    photoFile: 'tolu.jpg',
-  },
-  {
-    name: 'Faizaan Chishtie',
-    title: 'VP Academic',
-    tenure: '2018-Present',
-    photoFile: '',
-  },
-  {
-    name: 'Anthony A. Nader',
-    title: 'VP Marketing',
-    tenure: '2016-Present',
-    photoFile: 'anthony.png',
-  },
-  {
-    name: 'Melody Habbouche',
-    title: 'VP Communication',
-    tenure: '2015-Present',
-    photoFile: 'default-female-photo.jpg',
-  },
-  {
-    name: 'Harsh Gupta',
-    title: 'VP Events',
-    tenure: '2018-Present',
-    photoFile: 'harsh.jpeg',
-  },
-  {
-    name: 'Amelie Khan',
-    title: 'VP Community Engagement',
-    tenure: '2017-Present',
-    photoFile: 'default-female-photo.jpg',
-  },
-  {
-    name: 'Munir Aljawahari',
-    title: 'Graphic Designer',
-    tenure: '2017-Present',
-    photoFile: 'munir.jpg',
-  },
-  {
-    name: 'Sagal Maxamud',
-    title: 'Academic Coordinator',
-    tenure: '2018-Present',
-    photoFile: 'default-female-photo.jpg',
-  },
-  {
-    name: 'Maulik Shah',
-    title: 'Event Coordinator',
-    tenure: '2018-Present',
-    photoFile: 'maulik.png',
-  },
-  {
-    name: 'Sanat Nayar',
-    title: 'Event Coordinator',
-    tenure: '2018-Present',
-    photoFile: '',
-  },
-  {
-    name: 'Ian Desson',
-    title: 'Web Master',
-    tenure: '2017-Present',
-    photoFile: '',
-  },
-  {
-    name: 'Timi Owoturo',
-    title: 'Web Director',
-    tenure: '2018-Present',
-    photoFile: '',
-  },
-  {
-    name: 'Owen Anderson',
-    title: 'Web Director',
-    tenure: '2018-Present',
-    photoFile: '',
-  },
-]
-const palette = createPalette ({
-  primary: {
-    main:"#990012"
-  },
-  secondary: {
-    main: '#545454',
-    light:'#cacaca'
-  }
-})
+class MembersGrid extends React.PureComponent {
 
-const typography = createTypography(palette, {
-  fontFamily: 'Raleway'
-})
-const theme = createMuiTheme({
-  palette: palette,
-  typography: typography
-});
+  currentMembers = [
+    {
+      name: 'Anushka Paliwal',
+      title: 'President',
+      tenure: '2015-Present',
+      photoFile: 'default-female-photo.jpg',
+    },
+    {
+      name: 'Tolu Olubode',
+      title: 'Vice President',
+      tenure: '2017-Present',
+      photoFile: 'tolu.jpg',
+    },
+    {
+      name: 'Faizaan Chishtie',
+      title: 'VP Academic',
+      tenure: '2018-Present',
+      photoFile: '',
+    },
+    {
+      name: 'Anthony A. Nader',
+      title: 'VP Marketing',
+      tenure: '2016-Present',
+      photoFile: 'anthony.png',
+    },
+    {
+      name: 'Melody Habbouche',
+      title: 'VP Communication',
+      tenure: '2015-Present',
+      photoFile: 'default-female-photo.jpg',
+    },
+    {
+      name: 'Harsh Gupta',
+      title: 'VP Events',
+      tenure: '2018-Present',
+      photoFile: 'harsh.jpeg',
+    },
+    {
+      name: 'Amelie Khan',
+      title: 'VP Community Engagement',
+      tenure: '2017-Present',
+      photoFile: 'default-female-photo.jpg',
+    },
+    {
+      name: 'Munir Aljawahari',
+      title: 'Graphic Designer',
+      tenure: '2017-Present',
+      photoFile: 'munir.jpg',
+    },
+    {
+      name: 'Sagal Maxamud',
+      title: 'Academic Coordinator',
+      tenure: '2018-Present',
+      photoFile: 'default-female-photo.jpg',
+    },
+    {
+      name: 'Maulik Shah',
+      title: 'Event Coordinator',
+      tenure: '2018-Present',
+      photoFile: 'maulik.png',
+    },
+    {
+      name: 'Sanat Nayar',
+      title: 'Event Coordinator',
+      tenure: '2018-Present',
+      photoFile: '',
+    },
+    {
+      name: 'Ian Desson',
+      title: 'Web Master',
+      tenure: '2017-Present',
+      photoFile: '',
+    },
+    {
+      name: 'Timi Owoturo',
+      title: 'Web Director',
+      tenure: '2018-Present',
+      photoFile: '',
+    },
+    {
+      name: 'Owen Anderson',
+      title: 'Web Director',
+      tenure: '2018-Present',
+      photoFile: '',
+    },
+  ]
 
-const memberProfileGenerator = (startIndex, endIndex) => (
-  currentMembers
-  .slice(startIndex, endIndex)
-  .map(function(member, index) {
+
+  memberProfileGenerator = (startIndex, endIndex) => (
+    this.currentMembers
+    .slice(startIndex, endIndex)
+    .map(function(member, index) {
+      return (
+        <Grid
+          item
+          key={index}
+          xs={12}
+          sm={6}
+          lg={4}
+          style={{
+            width: '30%',
+          }}
+        >
+          <MemberProfile
+            memberName={member.name}
+            memberTitle={member.title}
+            memberTenure={member.tenure}
+            photoFile={member.photoFile}
+          />
+        </Grid>
+      )
+    })
+  )
+
+
+  render(){
+    
+    var palette = createPalette ({
+      primary: {
+        main:"#990012"
+      },
+      secondary: {
+        main: '#545454',
+        light:'#cacaca'
+      }
+    });
+
+    var typography = createTypography(palette, {
+      fontFamily: 'Raleway'
+    });
+
+    var theme = createMuiTheme({
+      palette: palette,
+      typography: typography
+    });
     return (
-      <Grid
-        item
-        key={index}
-        xs={12}
-        sm={6}
-        lg={4}
-        style={{
-          width: '30%',
-        }}
-      >
-        <MemberProfile
-          memberName={member.name}
-          memberTitle={member.title}
-          memberTenure={member.tenure}
-          photoFile={member.photoFile}
-        />
-      </Grid>
-    )
-  })
-)
-
-
-const MembersGrid = () => (
-  <Grid 
-    container
-    direction="column"
-    alignItems="center"
-    style={{
-      textTransform:'uppercase'
-    }}
-  >
-  <Helmet>
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
-  </Helmet>
-  <MuiThemeProvider theme={theme}>
-    <Typography 
-      align="center" 
-      variant="title"
-      variant="title"
-      color="primary"
+    <Grid 
+      container
+      direction="column"
+      alignItems="center"
       style={{
-        fontSize:"3rem"
+        textTransform:'uppercase'
       }}
     >
-      Awesome team of execs
+    <Helmet>
+      <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+    </Helmet>
+    <MuiThemeProvider theme={theme}>
       <Typography 
         align="center" 
-        variant="subtitle1"
-        color="secondary"
+        variant="title"
+        color="primary"
         style={{
-          fontSize:'80%'
+          fontSize:"3rem"
         }}
       >
-      The "A-Team" for Short
+        Awesome team of execs
+        <Typography 
+          align="center" 
+          variant="subtitle1"
+          color="secondary"
+          style={{
+            fontSize:'80%'
+          }}
+        >
+        The "A-Team" for Short
+        </Typography>
       </Typography>
-    </Typography>
-    <br/>
-    <br/>
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      spacing={16}
-      style={{
-        width:'75%'
-      }}
-    >
-      {
-        memberProfileGenerator(0, 6)
-      }
+      <br/>
+      <br/>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={16}
+        style={{
+          width:'75%'
+        }}
+      >
+        {
+          this.memberProfileGenerator(0, 6)
+        }
+      </Grid>
+      <br/>
+      <br/>
+      <Typography
+        align="center"
+        variant="title"
+        color="primary"
+        style={{
+          fontSize:'3rem'
+        }}
+      >
+        Directors
+      </Typography>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={16}
+        style={{
+          width:'75%'
+        }}
+      >
+        {
+          this.memberProfileGenerator(6)
+        }
+      </Grid>
+      </MuiThemeProvider>
     </Grid>
-    <br/>
-    <br/>
-    <Typography
-      align="center"
-      variant="title"
-      color="primary"
-      style={{
-        fontSize:'3rem'
-      }}
-    >
-      Directors
-    </Typography>
-    <Grid
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-      spacing={16}
-      style={{
-        width:'75%'
-      }}
-    >
-      {
-        memberProfileGenerator(6)
-      }
-    </Grid>
-    </MuiThemeProvider>
-  </Grid>
-)
-export default MembersGrid
+    )
+  }
+}
+export default MembersGrid;
