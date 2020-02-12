91bd668cfcff50b17392fea30a8e4a37f8f4bd94
Daniel Shwan <dannyshwan@gmail.com>
2020-02-11 22:43:01 -0500

    added grid

--- src/components/courseAdvice.js
+++ src/components/courseAdvice.js
@@ -1,13 +1,97 @@
-import React from 'react'
-import '../resources/styles/courseAdvice.css'
+import React from 'react';
+import '../resources/styles/courseAdvice.css';
+import { withStyles } from '@material-ui/core/styles';
+import { Grid, Paper } from '@material-ui/core';
 
-const CourseAdvice = () => {
+const styles = theme =>({
+   root: {
+      background: '#ffa000',
+      color: 'whitesmoke',
+      border: '0.20rem solid #ffa000',
+      height: "12rem",
+      width: "12rem",
+      fontSize: '1.5rem',
+      cursor: 'pointer',
+      transition: "200ms linear",
+      textTransform: "uppercase",
+   }
+ });
+
+const CourseAdvice = (props) => {
+
+   const { classes } = props;
+
    return(
-      <div>
-         <h2>Course Advice</h2>
-         <p>hello</p>
+      <div id='courseAdvice'>
+         <h1>COURSE ADVICE</h1>
+         <div>
+            <div id='courseSelection'>
+               <Grid container spacing={24}>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+                  <Grid item xs={3} spacing={2}>
+                     <Paper hover className="Paper" classes={{ root: classes.root }}>
+                        paper
+                     </Paper>
+                  </Grid>
+               </Grid>
+            </div>
+         </div>
       </div>
    );
 }
 
-export default CourseAdvice
+export default withStyles(styles)(CourseAdvice)
