7bf682bca41dc6e9029df22fbfd63ae64b2e4d27
Melissa Wu <mwu070@uottawa.ca>
2020-03-04 12:27:26 -0500

    add text to advice.js

--- src/pages/advice.js
+++ src/pages/advice.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from '../components/layout';
-
 import '../resources/styles/advicePage.css';
+import GeneralAdvice from '../components/generalAdvice'
 
 const AdvicePage = () => (
   <Layout>
     <div id="banner">
       <h1>FIRST YEAR TIPS</h1>
+    </div>
+    <div>
+      <GeneralAdvice /> 
     </div>
   </Layout>
 );
